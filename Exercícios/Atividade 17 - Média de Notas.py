7a7c35c6f2d47da193064fbd09478728083a284e
Bruno <bruno_oliuza2008@hotmail.com>
2024-08-17 01:50:19 -0300

    new

--- Exercícios/Atividade 17 - Média de Notas.py
+++ Exercícios/Atividade 17 - Média de Notas.py
@@ -4,8 +4,11 @@ notas = 0
 
 while notas != -1:
     notas = float(input("Digite suas notas para saber a média:"))
-    divi += 1
-    soma = soma + notas
-    resul = soma / divi
 
-print("A média de suas notas é:", resul)
+    if notas > -1:
+        divi += 1
+        soma = soma + notas
+
+resul = soma / divi
+
+print("A média de suas notas é: %.2f" % resul)
---
+++ Exercícios/Atividade 18 - Sequência Fibonacci.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número para saber sua sequência"))
+num2 = num
+
+for i in range(num):
+
